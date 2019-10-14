3c2ea8857f6e572765d13b4a5e8a555618bab554
Marc Mueller <marcmueller@Marcs-MBP.fritz.box>
2019-10-14 19:34:29 +0200

    View Engine Init
    • Added View Engine (EJS)
    • Added rough draft of book detail view
    • Added initial CSS setup
    • Added Icons

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+// Static Files
+app.use(express.static('public'));
+
+// Parser
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+// View Engine
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-   res.send('Hello World')
+   res.render('book-details.ejs')
 })
 
-app.listen(3000, () => {
-    console.log('listening on 3000')
+app.listen(80, () => {
+    console.log('Listening on 80')
 })
